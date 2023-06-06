bad6f9cf1d2b66bcaebf439907196f24f7f89751
blubu <91750788+blubu@users.noreply.github.com>
2023-06-06 22:05:16 +0530

    added textbox and convert button

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ app = QApplication([])
 window = QMainWindow()
 window.setGeometry(400, 250, 500, 500)
 
+textbox = QLineEdit(window)
+textbox.setGeometry(150, 250, 150, 20)
+
+button = QPushButton("Convert", window)
+button.setGeometry(310, 250, 70, 20)
+
 window.show()
 
 app.exec()
