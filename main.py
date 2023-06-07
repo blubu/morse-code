1095623cbe055a77700d97fc046a7b359ed3d7f0
blubu <91750788+blubu@users.noreply.github.com>
2023-06-07 21:13:04 +0530

    added functionality to dropdown options

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ morse_code_dict = {
     '>': '.-.--', ' ': '/'
 }
 
+text_code_dict = {
+    '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
+    '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q',
+    '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z',
+    '-----': '0', '.----': '1', '..---': '2', '...--': '3', '....-': '4', '.....': '5', '-....': '6', '--...': '7',
+    '---..': '8', '----.': '9', '.-.-.-': '.', '--..--': ',', '..--..': '?', '.----.': "'", '-.-.--': '!', '-..-.': '/',
+    '-.--.': '(', '-.--.-': ')', '.-...': '&', '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-',
+    '..--.-': '_', '.-..-.': '"', '...-..-': '$', '.--.-.': '@', '-.-.---': '#', '-.-.-': '<', '.-.--': '>', '/': ' '
+}
+
+
 app = QApplication([])
 
 window = QMainWindow()
@@ -40,8 +51,22 @@ def morse_code(entered_text):
     return res
 
 
+def text_code(entered_text):
+    res = ''
+    text = entered_text.split(' / ')
+    for word in text:
+        letter = word.split()
+        for l in letter:
+            res += text_code_dict[l]
+        res += " "
+    return res
+
+
 def convert_button():
-    res = morse_code(textbox.text())
+    if dropdown.currentIndex() == 0:
+        res = morse_code(textbox.text())
+    else:
+        res = text_code(textbox.text())
     label.setText(res)
 
 
